3e28fe42a6b1613626f95c7e030eac0754dd0e93
fliinn <fliinnpox@gmail.com>
2016-12-02 00:19:15 -0800

    Basic sort of toolbar implementation

--- toolbar.cpp
+++ toolbar.cpp
@@ -1,6 +1,49 @@
 #include "toolbar.h"
+#include <QToolBar>
+#include <QAction>
+#include <QIcon>
+#include <iostream>
+using namespace std;
+Toolbar::Toolbar(QWidget *parent) : QWidget(parent) {
 
-Toolbar::Toolbar(QWidget *parent) : QWidget(parent)
-{
+//Create a horizontal group to add the buttons to
+    hozLayoutWidget = new QWidget(parent);
+    hozLayoutWidget-> setGeometry(QRect(375, 25, 240, 20));
+    hozLayout = new QHBoxLayout(hozLayoutWidget);
+    hozLayout->setObjectName(QStringLiteral("toolbar"));
+    hozLayout->setContentsMargins(0,0,0,0);
 
+//Create a button for each of the tooltypes
+    selector = new QPushButton(hozLayoutWidget);
+    selector->setText("Select");
+    selector->setObjectName(QStringLiteral("Select"));
+    hozLayout->addWidget(selector);
+
+    pen = new QPushButton(hozLayoutWidget);
+    pen->setText("Pen");
+    pen->setObjectName(QStringLiteral("Pen"));
+    hozLayout->addWidget(pen);
+
+    colorPicker = new QPushButton(hozLayoutWidget);
+    colorPicker->setText("Pick Color");
+    colorPicker->setObjectName(QStringLiteral("Pick Color"));
+    hozLayout->addWidget(colorPicker);
+
+    connect(pen, SIGNAL(clicked()), this, SLOT(handlePen()));
+    connect(selector, SIGNAL(clicked()), this, SLOT(handleSelector()));
+    connect(colorPicker, SIGNAL(clicked()), this, SLOT(handleColorPicker()));
+}
+
+//Internal slots for each tool
+void Toolbar::handlePen() {
+
+    toolSelected(FrameEditor::ToolType::PEN);
+}
+void Toolbar::handleSelector() {
+
+    toolSelected(FrameEditor::ToolType::SELECTION);
+}
+void Toolbar::handleColorPicker() {
+
+    toolSelected(FrameEditor::ToolType::PICK_COLOR);
 }
--- toolbar.h
+++ toolbar.h
@@ -2,6 +2,9 @@
 #define TOOLBAR_H
 
 #include <QWidget>
+#include <QButtonGroup>
+#include <QHBoxLayout>
+#include <QToolButton>
 
 #include "frameeditor.h"
 
@@ -11,10 +14,19 @@ class Toolbar : public QWidget
 public:
     explicit Toolbar(QWidget *parent = 0);
 
+    QWidget *hozLayoutWidget;
+    QHBoxLayout *hozLayout;
+    QPushButton *pen;
+    QPushButton *selector;
+    QPushButton *colorPicker;
+
 signals:
     void toolSelected(FrameEditor::ToolType tool);
 
-public slots:
+private slots:
+    void handlePen();
+    void handleSelector();
+    void handleColorPicker();
 };
 
 #endif // TOOLBAR_H
